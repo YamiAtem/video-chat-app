1c1327dbb134860a964d4f210cfe4f922631ff75
Atharva Mishra <63883108+YamiAtem@users.noreply.github.com>
2022-05-13 19:38:52 +0530

    Class 219

--- public/script.js
+++ public/script.js
@@ -6,6 +6,25 @@ var peer = new Peer(undefined, {
 });
 
 const user = prompt("Enter your name");
+const my_video = document.createElement("video")
+my_video.muted = true
+
+let my_stream
+navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: true
+}).then((stream) => {
+    my_stream = stream
+    addVideoStream(my_video, stream)
+})
+
+function addVideoStream(video, stream) {
+    video.srcObject =  stream
+    video.addEventListener("loadedmetadata", () => {
+        video.play()
+        $("#video_grid").append(video)
+    })
+}
 
 $(function () {
     $("#show_chat").click(function () {
